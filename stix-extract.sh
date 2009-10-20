40933f19a59560669dd67ca4b644aa60c1182dfb
Will Robertson <wspr81@gmail.com>
2009-10-20 22:25:04 +1030

    lots more omissions and fixes to the table extracting code
    (for stix2008 table)
    
    almost there

--- stix-extract.sh
+++ stix-extract.sh
@@ -12,7 +12,6 @@ awk '
 	ignoreusv["000E5"] = "\\aa"
 	ignoreusv["000E6"] = "\\ae"
 	ignoreusv["000F0"] = "\\dh"
-	
 	ignoreusv["000FE"] = "\\th   "
 	ignoreusv["00110"] = "\\DJ   "
 	ignoreusv["00111"] = "\\dj   "
@@ -27,6 +26,61 @@ awk '
 	ignoreusv["001A1"] = "\\ohorn       "
 	ignoreusv["001AF"] = "\\Uhorn       "
 	ignoreusv["001B0"] = "\\uhorn       "
+	
+	ignoreusv["002B9"] = "\\cprime                  "
+	ignoreusv["002BA"] = "\\cdprime                 "
+	ignoreusv["002BC"] = "\\rasp                    "
+	ignoreusv["002BD"] = "\\lasp                    "
+	
+	ignoreusv["027CB"] = "\\mathoverlaylongsolidus  "
+	
+	ignoreusv["0300A"] = "\\lAngle                  "
+	ignoreusv["0300B"] = "\\rAngle                  "
+	ignoreusv["0301A"] = "\\lBrack                  "
+	ignoreusv["0301B"] = "\\rBrack                  "
+	ignoreusv["0301E"] = "\\cjkdprimequote          "
+	
+	ignoreusv["0FE35"] = "\\overparen "
+	ignoreusv["0FE36"] = "\\underparen"
+	ignoreusv["0FE37"] = "\\overbrace "
+	ignoreusv["0FE38"] = "\\underbrace"
+	ignoreusv["0FFFD"] = "\\unknown   "
+	
+	ignoreusv["02153"] = "\\fraconethird            "
+	ignoreusv["02154"] = "\\fractwothirds           "
+	ignoreusv["02155"] = "\\fraconefifth            "
+	ignoreusv["02156"] = "\\fractwofifths           "
+	ignoreusv["02157"] = "\\fracthreefifths         "
+	ignoreusv["02158"] = "\\fracfourfifths          "
+	ignoreusv["02159"] = "\\fraconesixth            "
+	ignoreusv["0215A"] = "\\fracfivesixths          "
+	ignoreusv["0215B"] = "\\fraconeeighth           "
+	ignoreusv["0215C"] = "\\fracthreeeighths        "
+	ignoreusv["0215D"] = "\\fracfiveeighths         "
+	ignoreusv["0215E"] = "\\fracseveneights         "
+	
+	ignoreusv["02070"] = "\\supzero                 "
+	ignoreusv["02074"] = "\\supfour                 "
+	ignoreusv["02075"] = "\\supfive                 "
+	ignoreusv["02076"] = "\\supsix                  "
+	ignoreusv["02077"] = "\\supseven                "
+	ignoreusv["02078"] = "\\supeight                "
+	ignoreusv["02079"] = "\\supnine                 "
+	ignoreusv["0207D"] = "\\suplparen               "
+	ignoreusv["0207E"] = "\\suprparen               "
+	ignoreusv["0207F"] = "\\supn                    "
+	ignoreusv["02080"] = "\\subzero                 "
+	ignoreusv["02081"] = "\\subone                  "
+	ignoreusv["02082"] = "\\subtwo                  "
+	ignoreusv["02083"] = "\\subthree                "
+	ignoreusv["02084"] = "\\subfour                 "
+	ignoreusv["02085"] = "\\subfive                 "
+	ignoreusv["02086"] = "\\subsix                  "
+	ignoreusv["02087"] = "\\subseven                "
+	ignoreusv["02088"] = "\\subeight                "
+	ignoreusv["02089"] = "\\subnine                 "
+	ignoreusv["0208D"] = "\\sublparen               "
+	ignoreusv["0208E"] = "\\subrparen               "
 	
 	# READ LINE
 	if (  \
@@ -48,8 +102,14 @@ awk '
         substr(texname,0,5) != "\\text"    && \
         substr(texname,0,4) != "\\ipa"    && \
         substr(texname,0,5) != "\\tone"    && \
+        substr(texname,0,5) != "\\circled"    && \
         substr(texname,3,1) != " "    && \
-        description !~ /<reserved>/  \
+        description !~ /<reserved>/  && \
+        description !~ /box drawings/  && \
+        description !~ /crop mark/  && \
+        description !~ /dingbat circled/  && \
+        description !~ /dingbat negative circled/  && \
+        description !~ /quot/  \
       )
       {
 		# SAVE DATA FOR NEXT LINE READS
@@ -58,6 +118,13 @@ awk '
 
 		# FIXES
 		if (texname == "\\bigtriangleup           ") { class = "B" }
+		if (texname == "\\Vvert                   ") { class = "F" }
+		if (usv == "02995") { class = "O" }
+		if (usv == "02996") { class = "C" }
+		if (usv == "02982") { class = "B" }
+		if (usv == "022EF") { class = "N" }
+		if (usv == "0219C")                          { printtexname = "\\leftwavearrow           " }
+		if (usv == "0219D")                          { printtexname = "\\rightwavearrow          " }
 		if (texname == "\\upvarepsilon            ") { printtexname = "\\upepsilon               " }
 		if (texname == "\\upepsilon               ") { printtexname = "\\upvarepsilon            " }
 		if (texname == "\\mbfvarepsilon           ") { printtexname = "\\mbfepsilon              " }
@@ -71,6 +138,7 @@ awk '
 		if (texname == "\\mbfitsansvarepsilon     ") { printtexname = "\\mbfitsansepsilon        " }
 		if (texname == "\\mbfitsansepsilon        ") { printtexname = "\\mbfitsansvarepsilon     " }
 		if (texname == "\\colon                   ") { printtexname = "\\mathratio               " }
+		if (texname == "\\dots                    ") { printtexname = "\\unicodeellipsis         " }
 		if (texname == "\\cdots                   ") { printtexname = "\\unicodecdots            " }
 
 
