b07275ed5859da4a28cf499e913c882934b9900e
Will Robertson <wspr81@gmail.com>
2015-07-30 17:59:49 +0930

    fix up l3build for ctan bundling
    closes #295

--- build.lua
+++ build.lua
@@ -2,23 +2,20 @@
 
 module = "unicode-math"
 
---[[
-deps = {"../l3svn/l3kernel","../l3svn/l3packages/xparse"}
-checkdeps   = deps
-typesetdeps = deps
-unpackdeps  = deps
---]]
-
-excludefiles = {}
-
-sourcefiles = {"*.dtx","unicode-math-table.tex"}
-unpackfiles = {"unicode-math.dtx"}
+sourcefiles  = {"*.dtx","unicode-math-table.tex"}
+unpackfiles  = {"unicode-math.dtx"}
 installfiles = {"*.sty","unicode-math-table.tex"}
+typesetfiles = {"unicode-math.dtx","unimath-symbols.ltx"}
+docfiles     = {"unicode-math-doc.tex"}
+demofiles    = {"unimath-example.ltx"}
 
 checkopts   = " -interaction=errorstopmode -halt-on-error "
 
 checkengines = {"xetex","luatex"}
 stdengine  = "luatex"
+typesetexe = "xelatex"
+
+packtdszip = true
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
