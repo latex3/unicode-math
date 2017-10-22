6b9df5edbf581cabf2476444f2486f91d72eeddf
Will Robertson <wspr81@gmail.com>
2017-10-22 10:57:25 +1030

    update copyright year automatically

--- update-copyright.sh
+++ update-copyright.sh
@@ -1,16 +1,22 @@
 ## UPDATE-COPYRIGHT.SH
-#
+
+# 1.
+# Updates copyright for Will to the current year.
+
+YEAR=`date +%Y`
+sed -i'.tmp' 's/\(2006\)-[0-9]* /\1-'$YEAR' /' COPYRIGHT
+rm COPYRIGHT.tmp
+
+for ii in *.ins *.tex *.ltx *.dtx ; do
+
+echo $ii
+
+# 2.
 # Updates copyright statements between lines surrounded by:
 #    % /©
 #.   ...
 #.   % ©/
 # with the text in the file COPYRIGHT
-#
-# Updates the first line of the file to match the filename
-
-for ii in *.ins *.tex *.ltx *.dtx ; do
-
-echo $ii
 
 awk 'FNR==NR{ _[++d]=$0;next}
 /^% \/©$/{
@@ -21,6 +27,9 @@ awk 'FNR==NR{ _[++d]=$0;next}
 /^% ©\/$/{f=0}!f' COPYRIGHT $ii > tmpfile
 
 mv tmpfile $ii
+
+# 3.
+# Updates the first line of the file to match the filename
 
 sed -i'.tmp' "1s/.*/%%^^J%%  $ii  -  part of UNICODE-MATH \<github.com\/wspr\/unicode-math\>/" $ii
 
