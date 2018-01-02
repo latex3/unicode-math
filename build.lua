6deda077b3e5d8adca55a5da332687014067a99c
Will Robertson <wspr81@gmail.com>
2018-01-02 10:38:59 +0800

    add l3build setversion support

--- build.lua
+++ build.lua
@@ -7,12 +7,59 @@ installfiles = {"*.sty","unicode-math-table.tex"}
 typesetfiles = {"*.ltx"}
 docfiles     = {"um-doc-*.tex"}
 textfiles    = {"*.md","LICENSE"}
+versionfiles = {"unicode-math.dtx","CHANGES.md"}
 
 checkengines = {"xetex","luatex"}
 typesetexe = "xelatex"
 
 packtdszip = true
 recordstatus = true
+
+
+-- [2017/11/18 v0.8i Unicode maths in XeLaTeX and LuaLaTeX]
+
+changeslisting = nil
+do
+  local f = assert(io.open("CHANGES.md", "r"))
+  changeslisting = f:read("*all")
+  f:close()
+end
+pkgversion = string.match(changeslisting,"- (%S+) %(.*%)")
+print('Current version (from first entry in CHANGES.md): '..pkgversion)
+
+local changesversion = true
+
+function setversion_update_line(line, date, version)
+  local lineorig = line
+  local date = string.gsub(date, "%-", "/")
+
+  if string.match(line, "%[%d%d%d%d/%d%d/%d%d %S+ .*%]") then
+    print("Found line: (unicode-math.dtx)")
+    print(line)
+    line = line:gsub("%d%d%d%d/%d%d/%d%d", date)
+    line = line:gsub("%[(%d%d%d%d/%d%d/%d%d) (%S+) (.*)%]", "["..date.." "..pkgversion.." %3]")
+  end
+
+  if changesversion and string.match(line, "- (%S+) %(.*%)") then
+    changesversion = false
+    print("Found line: (CHANGES.md)")
+    print(line)
+    line = line:gsub("- (%S+) %(.*%)","- %1 ("..date..")")
+  end
+
+  if not(line==lineorig) then
+		print("Replaced with line:")
+		print(line)
+  end
+
+  return line
+end
+
+
+
+--[[
+     MANIFEST SETTINGS
+--]]
 
 filematches = {}
 filematches["CHANGES.md"] = "Chronological list of release notes"
