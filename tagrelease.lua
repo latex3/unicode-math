699f05d479ed3fa321370f85675adc5908295837
Will Robertson <wspr81@gmail.com>
2018-01-15 12:50:51 +1030

    combine scripts

--- tagrelease.lua
+++ tagrelease.lua
@@ -2,16 +2,38 @@
 
 changeslisting = nil
 do
+  local f = assert(io.open("CHANGES.md", "r"))
+  changeslisting = f:read("*all")
+  f:close()
+end
+
+currentchanges = string.match(changeslisting,"(## %S+ %(.-%).-)%s*## %S+ %(.-%)")
+
+do
+  local f = assert(io.open("CHANGES-NEW.md", "w"))
+  f:write(currentchanges)
+  f:close()
+end
+
+changeslisting = nil
+do
   local f = assert(io.open("CHANGES-NEW.md", "r"))
   changeslisting = f:read("*all")
   f:close()
 end
+
+print("******************")
+print(changeslisting)
+print("******************")
+
 pkgversion = string.match(changeslisting,"## (%S+) %(.-%)")
-print('Current version (from entry in CHANGES-NEW.md): '..pkgversion)
+print('Current version: '..pkgversion)
 
 print('Current tag:')
 os.execute('git tag --contains | head -n1')
 
 gitcmd = 'git tag -a \''..pkgversion..'\' -F CHANGES-NEW.md'
-print(gitcmd)
+print('Tag command: "'..gitcmd..'"')
 os.execute(gitcmd)
+
+os.execute('rm CHANGES-NEW.md')
